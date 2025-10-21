9663b124d432ba93e5527437a4ca5ca640155360
Richard <155915123+richardhamwi@users.noreply.github.com>
2025-10-21 19:56:49 +1100

    Updated to complete job simulation

---
+++ dBtest/Program.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+const string connectionUri = "mongodb+srv://richhamwi:JrEKzJBM9CLDMrs7@cluster0.favm3lk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try
+{
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
